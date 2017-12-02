7e154433029706d04e3435ae01ec136d166d77cf
Jeffrey Phelps <phelpsjeffrey@yahoo.com>
2017-12-02 08:19:32 -0700

    Add files via upload
    Initial framework upload

---
+++ script.js
@@ -0,0 +1,14 @@
+// Jeffrey Phelps - DU Web Dev Bootcamp 2017/2018 - Week-5 Homework - Trivia Game
+
+// Setting up game audio
+
+
+
+// Creating random background music
+
+
+
+// Wait until the document loads to...
+
+
+
